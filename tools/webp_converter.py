fec9a44ce45ac2afdb15e5bf4be4e49b05bfe48b
DoppiaP <paulthementalistduo@gmail.com>
2026-08-30 12:08:18 +0200

    fix modifica

--- tools/webp_converter.py
+++ tools/webp_converter.py
@@ -1,7 +1,7 @@
 from PIL import Image
 
 # Usa ../ per tornare alla cartella principale, e assicurati di usare .png
-img = Image.open('../public/images/point_.png')
+img = Image.open('../public/images/point_.jpg')
 
 # Salva il file nella stessa cartella
 img.save('../public/images/point_.webp', 'webp', optimize=True, quality=80)
